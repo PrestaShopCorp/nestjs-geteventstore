40e66d4cf6a01ba22cf2a095045a14018c95e861
maz-dev <michael.mazurczak@gmail.com>
2020-02-13 13:33:51 +0100

    retryable in event store observer

--- src/event-store/event-store.observer.ts
+++ src/event-store/event-store.observer.ts
@@ -89,8 +89,103 @@ export class EventStoreObserver {
       );
   }
 
-  initAutoRetry() {}
+  initAutoRetry() {
+    merge(interval(this.retryInterval), this.retryTrigger)
+      .pipe(
+        filter(() => !this.retryInProgress),
+        filter(() => this.retryBuffer.length > 0),
+      )
+      .subscribe(() => this.retry(), () => {}, () => {});
+  }
 
+  private retry() {
+    const totalEvents = this.retryBuffer.length;
+
+    // This is not very atomic :astonished: :nuclear:
+    this.retryInProgress = true;
+    const buffer = fp.take(100)(this.retryBuffer);
+    this.retryBuffer = fp.drop(100)(this.retryBuffer);
+
+    let errorReceived = false;
+
+    from(buffer)
+      .pipe(
+        map(event => {
+          // This map returns an observable because we will use it with a concatAll()
+          return defer(() => {
+            // If an error occurred in the batch, we don't want to call the eventstore again,
+            // we want to re-queue events in the buffer and wait for the next retry call.
+            if (errorReceived) {
+              return of(event);
+            }
+
+            const stream = this._getStreamName(event);
+
+            // Try to write events in eventstore...
+            return this.eventstoreConnector.writeEvents(stream, event).pipe(
+              // ...with a timeout
+              timeout(this.writeTimeout),
+              // On success, we empty the returned observable because we dont want to
+              // requeue events in the buffer
+              mergeMapTo(EMPTY),
+              // On error, we want to requeue the event in the buffer and avoid the next
+              // events to be written in the eventstore.
+              tap(
+                null,
+                err => {
+                  console.log(
+                    `Unable to persist event to eventstore in stream ${stream} : ${JSON.stringify(
+                      event,
+                    )}`,
+                  );
+                },
+                null,
+              ),
+              catchError(err => {
+                if (this.isRetryable(err)) {
+                  errorReceived = true;
+                  return of(event);
+                }
+                console.log(
+                  `Dropping event because its invalid : ${JSON.stringify(
+                    event,
+                  )}`,
+                );
+                return EMPTY;
+              }),
+            );
+          });
+        }),
+        // Execute all previously defined commands sequentially, one by one
+        concatAll(),
+        // At this point, the observable contains only events not written in the eventstore.
+        // Increment the retry count in the event metadata
+        map(this._incrementRetryCount),
+        // Re-combine events to a single array
+        toArray(),
+      )
+      .subscribe(
+        events => {
+          // prepend events to the buffer
+          if (events.length > 0) {
+            this.retryBuffer = fp.concat(events, this.retryBuffer);
+            this.retryInProgress = false;
+            return;
+          }
+          // If there was no error (no events to put back into the buffer) and that
+          // some events are waiting in the buffer, trigger immediatly a retry to dequeue
+          // waiting events as soon as possible
+          this.retryInProgress = false;
+          this.retryTrigger.next(true);
+        },
+        err => {
+          console.log('Unexpected error in eventstore retry'); // <-- should not happen !
+          console.log(`ERROR: ${err.message || err}`);
+          this.retryInProgress = false;
+        },
+        null,
+      );
+  }
   private isRetryable(err) {
     const errCode = fp.getOr(200, 'response.status', err);
     return errCode !== 400;

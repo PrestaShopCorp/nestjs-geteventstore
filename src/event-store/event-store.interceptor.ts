42f757a5a53ef268e3b3676450a16e8fd4c64e50
Vincent Vermersch <vermersch@gmail.com>
2020-03-04 14:34:11 +0100

    Correlation ID on eventstore

--- src/event-store/event-store.interceptor.ts
+++ src/event-store/event-store.interceptor.ts
@@ -1,24 +1,47 @@
-import {
-  NestInterceptor,
-  Injectable,
-  ExecutionContext,
-  CallHandler,
-  Inject,
-} from '@nestjs/common';
+import { CallHandler, ExecutionContext, Inject, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable, Subject } from 'rxjs';
 import { EventStoreObserver } from './event-store.observer';
 import { EVENT_STORE_OBSERVER_TOKEN } from './shared/constants';
+import * as express from 'express';
+import { filter, map } from 'rxjs/operators';
+import { EventStoreEvent } from '..';
 
 @Injectable()
 export class EventStoreInterceptor implements NestInterceptor {
   constructor(
     @Inject(EVENT_STORE_OBSERVER_TOKEN)
     private readonly eventStoreOberver: EventStoreObserver,
-  ) {}
+  ) {
+  }
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // Extract correlation from request
+    const req: express.Request = context.switchToHttp().getRequest();
+    const correlationId = req.header('x-correlation-id');
+
+    // Build router to send data to next and Eventstore
     const handlerSubject$ = new Subject();
+
+    // Add correlation id to the event
+    // only if return is an event
+    handlerSubject$
+      .pipe(
+        filter(req => {
+          return req instanceof EventStoreEvent;
+        }),
+        map((ev: EventStoreEvent) => {
+          if (correlationId) {
+            ev.metadata['correlation_id'] = correlationId;
+          }
+          return ev;
+        }),
+      )
+      .subscribe(this.eventStoreOberver);
+
+    // Run next step and send it to everyone
     next.handle().subscribe(handlerSubject$);
-    handlerSubject$.subscribe(this.eventStoreOberver);
+
+    // return next result
     return handlerSubject$;
   }
 }

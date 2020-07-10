a11f30cd2cd9956cab1ace757a7ec64f841f69fa
Vincent Vermersch <vermersch@gmail.com>
2020-07-10 11:55:55 +0200

    add example for connection params and logging

--- examples/src/heroes/event-store-heroes.module.ts
+++ examples/src/heroes/event-store-heroes.module.ts
@@ -10,6 +10,7 @@ import { EventStoreCqrsModule, IEventStoreEventOptions } from '../../../src';
 import { HealthController } from './health.controller';
 import { TerminusModule } from '@nestjs/terminus';
 import { LoggerModule } from 'nestjs-pino-stackdriver/dist';
+import * as util from 'util';
 
 @Module({
   imports: [
@@ -29,8 +30,42 @@ import { LoggerModule } from 'nestjs-pino-stackdriver/dist';
           port: +process.env.EVENTSTORE_HTTP_PORT || 22113,
         },
         tcpConnectionName: 'connection-hero-event-handler-and-saga',
+        options: {
+          log: {
+            debug: (str, ...args) => {
+              Logger.warn(util.format(str, ...args), 'EventStoreCore');
+            },
+            info: (str, ...args) => {
+              Logger.log(util.format(str, ...args), 'EventStoreCore');
+            },
+            error: (str, ...args) => {
+              Logger.log(util.format(str, ...args), 'EventStoreCore');
+            },
+          },
+          // Buffer events if remote is slow or not available
+          maxQueueSize: 100_000,
+          maxRetries: 10_000,
+          operationTimeout: 5_000,
+          operationTimeoutCheckPeriod: 1_000,
+          // Fail fast on connect
+          clientConnectionTimeout: 2_000,
+          failOnNoServerResponse: true,
+          // Try to reconnect every 10s for 30mn
+          maxReconnections: 200,
+          reconnectionDelay: 10_000,
+          // Production heartbeat
+          heartbeatInterval: 10_000,
+          heartbeatTimeout: 3_000,
+        },
+        onTcpDisconnected: () => {
+          Logger.error(
+            `Connection to eventstore lost`,
+            undefined,
+            'EventStoreCore',
+          );
+          //process.exit(137);
+        },
         onTcpConnected: () => {},
-        onTcpDisconnected: () => {},
       },
       {
         eventMapper: (data, options: IEventStoreEventOptions) => {

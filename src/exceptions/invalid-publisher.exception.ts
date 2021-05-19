68c0a7af5dd4243462a6b52947d7258e6bca54ca
Jacob Dharandas <jacob.dharandas@prestashop.com>
2021-05-19 15:31:12 +0200

    Added default "publishAll" for addPublisher

---
+++ src/exceptions/invalid-publisher.exception.ts
@@ -0,0 +1,14 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class InvalidPublisherException<
+  T extends object = Function
+> extends HttpException {
+  constructor(publisher: T, method: keyof T) {
+    super(
+      `Invalid publisher: expected ${
+        publisher.constructor.name + '::' + method
+      } to be a function`,
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}

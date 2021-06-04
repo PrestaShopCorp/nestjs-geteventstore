47fb49b56dd5cdc310fa1649de5152e1c59650ea
Jacob Dharandas <jacob.dharandas@prestashop.com>
2021-06-04 11:15:29 +0200

    Added decorator EventVersion

--- examples/src/heroes/events/impl/hero-killed-dragon.event.ts
+++ examples/src/heroes/events/impl/hero-killed-dragon.event.ts
@@ -1,5 +1,8 @@
 import { EventStoreAcknowledgeableEvent } from '../../../../../src';
+import { EventVersion } from '../../../../../src/decorators/event-version.decorator';
 
+// This is the second version of this event
+@EventVersion(2)
 export class HeroKilledDragonEvent extends EventStoreAcknowledgeableEvent {
   public declare readonly data: {
     heroId: string;
---
+++ src/decorators/event-version.decorator.ts
@@ -0,0 +1,18 @@
+import { IBaseEvent } from '../interfaces';
+
+export const EventVersion = (version: number) => <
+  T extends { new (...args: any[]): IBaseEvent }
+>(
+  BaseEvent: T,
+) => {
+  const newClass = class extends BaseEvent implements IBaseEvent {
+    constructor(...args: any[]) {
+      super(...args);
+      this.metadata.version = version;
+    }
+  };
+  Object.defineProperty(newClass, 'name', {
+    value: BaseEvent.name,
+  });
+  return newClass;
+};

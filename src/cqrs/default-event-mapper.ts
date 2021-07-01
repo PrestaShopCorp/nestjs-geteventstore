1aab9546a7d0981bbbfb77e37a05d22d783aabab
Jacob Dharandas <jacob.dharandas@prestashop.com>
2021-07-01 18:26:52 +0200

    Updated nestjs-context

--- src/cqrs/default-event-mapper.ts
+++ src/cqrs/default-event-mapper.ts
@@ -5,7 +5,6 @@ export const defaultEventMapper = (
   allEvents: ReadEventBusConfigType['allowedEvents'],
 ) => {
   const logger = new Logger('Default Event Mapper');
-  logger.log(`Will build events from ${Object.keys(allEvents).join(', ')}`);
   return ((data, options: ReadEventOptionsType) => {
     let className = `${options.eventType}`;
     if (allEvents[className]) {

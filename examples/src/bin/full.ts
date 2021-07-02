33d031dbfb64f8016b30352caac6b7c3abfc2408
Pedro Pérez Furio <perez-furio.ext@prestashop.com>
2021-07-02 08:21:25 +0200

    Yarn update and dependecy cleanup

--- examples/src/bin/full.ts
+++ examples/src/bin/full.ts
@@ -1,19 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AllExceptionFilter } from '../all-exception.filter';
 import { Logger } from 'nestjs-pino-stackdriver';
+import { AllExceptionFilter } from '../all-exception.filter';
 import { EventStoreHeroesModule } from '../heroes/event-store-heroes.module';
-
-declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(EventStoreHeroesModule.register(), {
     logger: new Logger(),
   });
 
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
   app.useGlobalFilters(new AllExceptionFilter());
   await app.listen(3000, () =>
     console.log('Application is listening on port 3000.'),

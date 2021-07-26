8c155dcf441bdd9e7ef5b7c657b47695c613fb87
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2021-07-26 14:01:12 +0200

    added some linting conf for examples dependencies

--- examples/src/event-store-usages/persistent-subscription/persistent-subscription.module.ts
+++ examples/src/event-store-usages/persistent-subscription/persistent-subscription.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import PersistantSubscriptionController from './persistant-subscription.controller';
+import { IEventStoreConfig } from '@nestjs-geteventstore/event-store/config';
+import { GrpcEventStoreConfig } from "@nestjs-geteventstore/event-store/config/grpc/grpc-event-store-config";
+import { EventStoreModule } from "@nestjs-geteventstore/event-store/event-store.module";
+
+const eventStoreConfig: GrpcEventStoreConfig = {
+  connectionSettings: {
+    connectionString:
+      process.env.CONNECTION_STRING || 'esdb://localhost:20113?tls=false',
+  },
+  defaultUserCredentials: {
+    username: process.env.EVENTSTORE_CREDENTIALS_USERNAME || 'admin',
+    password: process.env.EVENTSTORE_CREDENTIALS_PASSWORD || 'changeit',
+  },
+};
 
 @Module({
-          controllers: [PersistantSubscriptionController],
-        })
-export default class PersistentSubscriptionModule {
-}
+  controllers: [PersistantSubscriptionController],
+  imports: [EventStoreModule.register(eventStoreConfig as IEventStoreConfig, {})],
+})
+export default class PersistentSubscriptionModule {}

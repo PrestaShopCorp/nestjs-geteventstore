7f29e9e76b46ec60be789e9ebaec9dae89f68304
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2021-10-22 11:13:17 +0200

    add some exports (#86)

--- src/event-store/config/index.ts
+++ src/event-store/config/index.ts
@@ -1,2 +1,3 @@
+export * from './connector';
 export * from './event-store-connection-config';
 export * from './event-store-service-config.interface';
--- src/event-store/index.ts
+++ src/event-store/index.ts
@@ -1,3 +1,4 @@
 export * from './publisher/event-store.publisher';
+export * from './config';
 export * from './services/event-store.service';
 export * from './event-store-aggregate-root';

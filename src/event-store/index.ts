c37dd73df1847d1f1722d911fb2186a8290ef210
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2022-02-10 17:02:07 +0100

    add an export

--- src/event-store/index.ts
+++ src/event-store/index.ts
@@ -1,4 +1,5 @@
 export * from './publisher/event-store.publisher';
 export * from './config';
+export * from './health';
 export * from './services/event-store.service';
 export * from './event-store-aggregate-root';
